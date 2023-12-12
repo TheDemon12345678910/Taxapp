198142d065fa6216f60a02e48ac924b85d092038
Ane Hoimark <anehoimark@gmail.com>
2023-12-12 15:07:49 +0100

    updated imports

--- frontend/src/interceptors/AuthHttpInterceptor.ts
+++ frontend/src/interceptors/AuthHttpInterceptor.ts
@@ -1,3 +1,8 @@
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { TokenService } from "src/services/token.service";
+
 @Injectable()
 export class AuthHttpInterceptor implements HttpInterceptor {
   constructor(private readonly service: TokenService) {}
